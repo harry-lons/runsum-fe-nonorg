b494d68d796dada5732406fee0528eaf6e18b0ac
harry-lons <harrylonsdale25@gmail.com>
2025-05-15 10:27:46 -0700

    adjust start date on all-time

--- src/pages/fcts/functions.js
+++ src/pages/fcts/functions.js
@@ -75,12 +75,20 @@ export async function getAllInfo(accessToken, MAX_CONCURRENT_REQUESTS) {
         console.error('Error fetching activities:', error);
         return false;
     }
+    let firstActivityDate = allActivities.length > 0 ? allActivities[allActivities.length - 1].start_date_local : null;
+    if(localStorage.getItem('startDate') == new Date(2000, 0, 1)) {
+        // reset start date based on earliest activity
+        console.log("jan12000");
+        localStorage.setItem('startDate', new Date(firstActivityDate).toISOString());
+    }
+
     try {
         const payload = {
             allSports: await extractAllSportMetrics(allActivities, accessToken),
             run: await extractRunMetrics(allActivities, accessToken),
             ride: await extractBikeMetrics(allActivities, accessToken),
             swim: await extractSwimMetrics(allActivities, accessToken),
+            firstActivityDate: allActivities.length > 0 ? allActivities[allActivities.length - 1].start_date_local : null,
         };
         console.log(payload);
         return payload;
